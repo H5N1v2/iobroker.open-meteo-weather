bbe0b9983c50fe7c21d426a8c957a345a065c819
H5N1v2 <crazybastards@web.de>
2026-01-26 20:54:55 +0100

    feat: Expand unit definitions and add translations for pollen values.

--- build/lib/units.js
+++ build/lib/units.js
@@ -19,7 +19,8 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 var units_exports = {};
 __export(units_exports, {
   unitMapImperial: () => unitMapImperial,
-  unitMapMetric: () => unitMapMetric
+  unitMapMetric: () => unitMapMetric,
+  unitTranslations: () => unitTranslations
 });
 module.exports = __toCommonJS(units_exports);
 const unitMapMetric = {
@@ -30,13 +31,22 @@ const unitMapMetric = {
   wind_speed: "km/h",
   wind_gusts: "km/h",
   pm: "\xB5g/m\xB3",
+  dust: "\xB5g/m\xB3",
+  carbon_monoxide: "\xB5g/m\xB3",
+  ozone: "\xB5g/m\xB3",
   cloud: "%",
   wind_direction: "\xB0",
   dew_point: "\xB0C",
   pressure: "hPa",
   sunshine: "h",
   snowfall: "cm",
-  snow_depth: "cm"
+  snow_depth: "cm",
+  alder_pollen: "grains/m\xB3",
+  birch_pollen: "grains/m\xB3",
+  grass_pollen: "grains/m\xB3",
+  mugwort_pollen: "grains/m\xB3",
+  ragweed_pollen: "grains/m\xB3",
+  olive_pollen: "grains/m\xB3"
 };
 const unitMapImperial = {
   temperature: "\xB0F",
@@ -46,6 +56,9 @@ const unitMapImperial = {
   wind_speed: "mph",
   wind_gusts: "mph",
   pm: "\xB5g/m\xB3",
+  dust: "\xB5g/m\xB3",
+  carbon_monoxide: "\xB5g/m\xB3",
+  ozone: "\xB5g/m\xB3",
   cloud: "%",
   wind_direction: "\xB0",
   dew_point: "\xB0F",
@@ -54,9 +67,60 @@ const unitMapImperial = {
   snowfall: "inch",
   snow_depth: "inch"
 };
+const unitTranslations = {
+  de: {
+    "grains/m\xB3": "K\xF6rner/m\xB3",
+    "grains/m3": "K\xF6rner/m\xB3"
+  },
+  en: {
+    "grains/m\xB3": "grains/m\xB3",
+    "grains/m3": "grains/m3"
+  },
+  pl: {
+    "grains/m\xB3": "ziaren/m\xB3",
+    "grains/m3": "ziaren/m3"
+  },
+  uk: {
+    "grains/m\xB3": "\u0437\u0435\u0440\u043D\u0430/m\xB3",
+    "grains/m3": "\u0437\u0435\u0440\u043D\u0430/m3"
+  },
+  pt: {
+    "grains/m\xB3": "gr\xE3os/m\xB3",
+    "grains/m3": "gr\xE3os/m3"
+  },
+  zh: {
+    "grains/m\xB3": "\u9897/\u7ACB\u65B9\u7C73",
+    "grains/m3": "\u9897/\u7ACB\u65B9\u7C73"
+  },
+  cn: {
+    "grains/m\xB3": "\u9897/\u7ACB\u65B9\u7C73",
+    "grains/m3": "\u9897/\u7ACB\u65B9\u7C73"
+  },
+  ru: {
+    "grains/m\xB3": "\u0437\u0435\u0440\u043D\u0430/m\xB3",
+    "grains/m3": "\u0437\u0435\u0440\u043D\u0430/m3"
+  },
+  fr: {
+    "grains/m\xB3": "grains/m\xB3",
+    "grains/m3": "grains/m3"
+  },
+  it: {
+    "grains/m\xB3": "grani/m\xB3",
+    "grains/m3": "grani/m3"
+  },
+  nl: {
+    "grains/m\xB3": "korrels/m\xB3",
+    "grains/m3": "korrels/m3"
+  },
+  es: {
+    "grains/m\xB3": "granos/m\xB3",
+    "grains/m3": "granos/m3"
+  }
+};
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   unitMapImperial,
-  unitMapMetric
+  unitMapMetric,
+  unitTranslations
 });
 //# sourceMappingURL=units.js.map
--- src/lib/units.ts
+++ src/lib/units.ts
@@ -8,6 +8,9 @@ export const unitMapMetric: Record<string, string> = {
 	wind_speed: 'km/h',
 	wind_gusts: 'km/h',
 	pm: 'µg/m³',
+	dust: 'µg/m³',
+	carbon_monoxide: 'µg/m³',
+	ozone: 'µg/m³',
 	cloud: '%',
 	wind_direction: '°',
 	dew_point: '°C',
@@ -15,6 +18,12 @@ export const unitMapMetric: Record<string, string> = {
 	sunshine: 'h',
 	snowfall: 'cm',
 	snow_depth: 'cm',
+	alder_pollen: 'grains/m³',
+	birch_pollen: 'grains/m³',
+	grass_pollen: 'grains/m³',
+	mugwort_pollen: 'grains/m³',
+	ragweed_pollen: 'grains/m³',
+	olive_pollen: 'grains/m³',
 };
 
 export const unitMapImperial: Record<string, string> = {
@@ -25,6 +34,9 @@ export const unitMapImperial: Record<string, string> = {
 	wind_speed: 'mph',
 	wind_gusts: 'mph',
 	pm: 'µg/m³',
+	dust: 'µg/m³',
+	carbon_monoxide: 'µg/m³',
+	ozone: 'µg/m³',
 	cloud: '%',
 	wind_direction: '°',
 	dew_point: '°F',
@@ -32,4 +44,55 @@ export const unitMapImperial: Record<string, string> = {
 	sunshine: 'h',
 	snowfall: 'inch',
 	snow_depth: 'inch',
+};
+
+export const unitTranslations: Record<string, Record<string, string>> = {
+	de: {
+		'grains/m³': 'Körner/m³',
+		'grains/m3': 'Körner/m³',
+	},
+	en: {
+		'grains/m³': 'grains/m³',
+		'grains/m3': 'grains/m3',
+	},
+	pl: {
+		'grains/m³': 'ziaren/m³',
+		'grains/m3': 'ziaren/m3',
+	},
+	uk: {
+		'grains/m³': 'зерна/m³',
+		'grains/m3': 'зерна/m3',
+	},
+	pt: {
+		'grains/m³': 'grãos/m³',
+		'grains/m3': 'grãos/m3',
+	},
+	zh: {
+		'grains/m³': '颗/立方米',
+		'grains/m3': '颗/立方米',
+	},
+	cn: {
+		'grains/m³': '颗/立方米',
+		'grains/m3': '颗/立方米',
+	},
+	ru: {
+		'grains/m³': 'зерна/m³',
+		'grains/m3': 'зерна/m3',
+	},
+	fr: {
+		'grains/m³': 'grains/m³',
+		'grains/m3': 'grains/m3',
+	},
+	it: {
+		'grains/m³': 'grani/m³',
+		'grains/m3': 'grani/m3',
+	},
+	nl: {
+		'grains/m³': 'korrels/m³',
+		'grains/m3': 'korrels/m3',
+	},
+	es: {
+		'grains/m³': 'granos/m³',
+		'grains/m3': 'granos/m3',
+	},
 };
